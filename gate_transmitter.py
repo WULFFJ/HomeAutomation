786c0ff2fc47634c18db3e3f25b9975ec99d0019
James <78693214+WULFFJ@users.noreply.github.com>
2024-04-16 23:08:48 -0400

    Update gate_transmitter.py

--- gate_transmitter.py
+++ gate_transmitter.py
@@ -5,30 +5,34 @@ import digitalio
 import time
 
 # Define the contact sensor pin
-contact_sensor = digitalio.DigitalInOut(board.D3)
+contact_sensor = digitalio.DigitalInOut(board.D4)
 contact_sensor.direction = digitalio.Direction.INPUT
 contact_sensor.pull = digitalio.Pull.UP  # Use a pull-up resistor
 
-pin = digitalio.DigitalInOut(board.TX)
+# Define the output pin
+pin = digitalio.DigitalInOut(board.D6)
 pin.direction = digitalio.Direction.OUTPUT
 
-message = 'xxxxxxxxxxxxx' #1's and 0's here this will server as your passcode
-sync = 'xxxxxxxxxx' #1's and 0's here'   This is the small code to wake up the receiver and synch the timing
+message = 'xxxxxxx' #1's and 0's here this will server as your passcode
+sync = 'xxxxxxxxxxx' #1's and 0's here'   This is the small code to wake up the receiver and synch the timing
 
 while True:
-    if contact_sensor.value:
+    # Check if the contact sensor is detected
+    if not contact_sensor.value:  # Changed this line
+        # Send the sync signal
         for bit in sync:
             pin.value = True if bit == '1' else False
             time.sleep(0.002)  # Adjusted timing
-            if not contact_sensor.value:  # Check if the sensor value has changed
-                break
 
-        if contact_sensor.value:  # Check again before sending the message
-            for bit in message:
-                pin.value = True if bit == '1' else False
-                time.sleep(0.002)  # Adjusted timing
-                if not contact_sensor.value:  # Check if the sensor value has changed
-                    break
+        # Send the message
+        for bit in message:
+            pin.value = True if bit == '1' else False
+            time.sleep(0.002)  # Adjusted timing
+    else:
+        # Print a message if the contact sensor is not detected
+        print("Contact sensor is not detected.")
+        # Sleep for 1 second if the sensor is not detected
+        time.sleep(1)
     else:
         # Sleep for 1 second if the sensor is not detected
         time.sleep(1)
